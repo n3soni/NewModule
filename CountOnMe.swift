91a66fac854b1d52867a05e93780ec857859a168
Nitin Soni <nitin.soni4@t-mobile.com>
2021-04-12 00:11:15 +0530

    initial commit

---
+++ CountOnMe.swift
@@ -0,0 +1,14 @@
+//
+//  CountOnMe.swift
+//  NewModule
+//
+//  Created by Nitin Soni on 11/04/21.
+//
+
+import Foundation
+
+public struct CountOnMe {
+    public static func countingStartsWith() {
+        print("Counting starts with 1")
+    }
+}
---
+++ Example/Pods/Target Support Files/NewModule/NewModule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NewModule : NSObject
+@end
+@implementation PodsDummy_NewModule
+@end
